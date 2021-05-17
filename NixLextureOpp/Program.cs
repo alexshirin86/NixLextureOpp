e61231ef4bf60edb78cbe1cb04823e15782151af
Lucky <Lucky@LUCKY-PC>
2021-05-17 16:28:39 +0300

    Add project files.

---
+++ NixLextureOpp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NixLextureOpp
+{
+    class Program
+    {
+        class Person
+        {
+            public string name;
+            public int age; 
+
+            public void GetInfo()
+            {
+                Console.WriteLine(name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
